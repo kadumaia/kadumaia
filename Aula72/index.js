97acd3a576a2e2ad9b3b79ccab8bf2b7c680a5c1
Carlos Maia <kadu.mmaia@gmail.com>
2022-09-07 09:53:09 -0300

    Aula sobre getters e setter

--- Aula72/index.js
+++ Aula72/index.js
@@ -1,31 +1,47 @@
 // define property -- define properties -> getters e setter
 
 function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    let estoquePrivado = estoque;
     Object.defineProperty(this, 'estoque', {
-        enumerable: true, //mostra a chave
-        value: estoque, //valor
-        writable: true, //valor alterar
-        configurable: true  //configurável
-    });
-    Object.defineProperties(this, {
-        nome: {
-            enumerable: true, //mostra a chave
-            value: nome, //valor
-            writable: true, //valor alterar
-            configurable: true  //configurável
+        enumerable: true,
+        // value: estoque,
+        // writable: true,
+        configurable: true,
+        get: function () {
+            return estoquePrivado;
         },
-        preco: {
-            enumerable: true, //mostra a chave
-            value: preco, //valor
-            writable: true, //valor alterar
-            configurable: true  //configurável
-        },
+        set: function (valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('mensagem!');
+            }
+            estoquePrivado = valor;
+        }
     });
+
 }
 
-const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1);
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome;
+        },
+        set nome(valor) {
+            valor = valor.replace('coisa', 'cu');
+            nome = valor;
+        }
+    }
+}
 
-for (let chave in p1) {
-    console.log(chave);
-};
+
+const p1 = new Produto('camiseta', 20, 3);
+// p1.estoque = 'qualquer coisa'; // a geente utiliza getter e setter para arrumar os valores
+// console.log(p1);
+p1.estoque = 500;
+console.log(p1.estoque);
+
+const p2 = criaProduto('Camiseta!');
+p2.nome = 'qualquer outra coisa';
+console.log(p2.nome);
