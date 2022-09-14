e5679814ca38c9f3e27bd577f865665fe90444be
Carlos Maia <kadu.mmaia@gmail.com>
2022-09-14 15:32:01 -0300

    pequena atualizaçao na aula 72

--- Aula72/index.js
+++ Aula72/index.js
@@ -1,47 +1,71 @@
 // define property -- define properties -> getters e setter
 
-function Produto(nome, preco, estoque) {
+/**
+ * function Produto(nome, preco, estoque) {
+  this.nome = nome;
+  this.preco = preco;
+
+  let estoquePrivado = estoque;
+  Object.defineProperty(this, "estoque", {
+    enumerable: true,
+    // value: estoque,
+    // writable: true,
+    configurable: true,
+    get: function () {
+      return estoquePrivado;
+    },
+    set: function (valor) {
+      if (typeof valor !== "number") {
+        throw new TypeError("mensagem!");
+      }
+      estoquePrivado = valor;
+    },
+  });
+}
+ */
+
+class Produto {
+  constructor(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
 
     let estoquePrivado = estoque;
-    Object.defineProperty(this, 'estoque', {
-        enumerable: true,
-        // value: estoque,
-        // writable: true,
-        configurable: true,
-        get: function () {
-            return estoquePrivado;
-        },
-        set: function (valor) {
-            if (typeof valor !== 'number') {
-                throw new TypeError('mensagem!');
-            }
-            estoquePrivado = valor;
+    Object.defineProperty(this, "estoque", {
+      enumerable: true,
+      // value: estoque,
+      // writable: true,
+      configurable: true,
+      get: function () {
+        return estoquePrivado;
+      },
+      set: function (valor) {
+        if (typeof valor !== "number") {
+          throw new TypeError("mensagem!");
         }
+        estoquePrivado = valor;
+      },
     });
-
+  }
 }
 
 function criaProduto(nome) {
-    return {
-        get nome() {
-            return nome;
-        },
-        set nome(valor) {
-            valor = valor.replace('coisa', 'cu');
-            nome = valor;
-        }
-    }
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      valor = valor.replace("coisa", "cu");
+      nome = valor;
+    },
+  };
 }
 
-
-const p1 = new Produto('camiseta', 20, 3);
+const p1 = new Produto("camiseta", 20, 3);
 // p1.estoque = 'qualquer coisa'; // a geente utiliza getter e setter para arrumar os valores
 // console.log(p1);
 p1.estoque = 500;
 console.log(p1.estoque);
 
-const p2 = criaProduto('Camiseta!');
-p2.nome = 'qualquer outra coisa';
+const p2 = criaProduto("Camiseta!");
+p2.nome = "qualquer outra coisa";
 console.log(p2.nome);
